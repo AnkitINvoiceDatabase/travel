b5cd49bcbde0683b9cc2894ca76bc8e579214b20
anki.gohil <ankit.gohil@ncode.in>
2026-09-24 16:47:18 +0530

    update frunted

--- Program.cs
+++ Program.cs
@@ -17,18 +17,44 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
 
-// MVC routing
+//// MVC routing
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}");   ye publick hai 
+
+////app.MapControllerRoute(
+////    name: "default",
+////    pattern: "{controller=Admin}/{action=Dashboard}");
+//// API controllers   ye admin hai 
+///
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/" &&
+        context.Connection.LocalPort == 5017)
+    {
+        context.Response.Redirect("/Admin/Dashboard");
+        return;
+    }
+
+    await next();
+});
+
+app.MapControllerRoute(
+    name: "admin",
+    pattern: "Admin/{action=Dashboard}/{id?}",
+    defaults: new { controller = "Admin" });
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-// API controllers
 app.MapControllers();
 
 app.Run();
